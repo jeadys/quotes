89accb57c06e62489d2bbbf59d0e28fc2585c17b
Jeadys <yassin.ao.work@gmail.com>
2025-02-03 16:56:50 +0100

    add performance graph to canvas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./components/Experience.tsx";
 
+import { Perf } from "r3f-perf";
+
 function App() {
   return (
     <Canvas
@@ -12,6 +14,7 @@ function App() {
         position: [4, 15, 20],
       }}
     >
+      <Perf position="top-left" />
       <Experience />
     </Canvas>
   );
